0c7fdb26718242d72f23949b3ee90fc7a0311527
augustocsetti <augustocsetti@live.com>
2022-08-11 20:39:44 -0300

    updating parameters

--- tools/arg_parser.py
+++ tools/arg_parser.py
@@ -6,6 +6,6 @@ def build_parser(description = None) -> argparse.ArgumentParser:
     Function that returns a Argument Parser.
     '''
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("simulation_type", type=str, help=f"Insert a number (0, 1 or 2) representing a simulation type.")
+    parser.add_argument("simulation_type", type=str, help=f"Insert a number (0, 1, 2, 3...) representing a simulation type.")
 
     return parser
